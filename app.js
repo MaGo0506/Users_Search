c63c8bfb07c06d462515010ba0e4c283666849dd
Marko Goronjic <72967482+MaGo0506@users.noreply.github.com>
2021-06-17 04:11:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,75 @@
 const searchInput = document.querySelector('.js-searchInput'),
+      userData = document.querySelector('.js-userData'),
       matchList = document.querySelector('.js-matchList');
-let   webLink;
+let   webLink,
+      linkTag = document.querySelector('.js-linkTag'),
+      users = [];
+
 
 /**
-  * We are fetching users.json and filtering them
+  * Here we are filtering our users even if we type in lowercase
+  */
+searchInput.addEventListener('keyup', (e) => {
+    const targetValue = e.target.value.toLowerCase(),
+          filteredUsers =  users.filter((user) => {
+            return user.username.toLowerCase().startsWith(targetValue);
+    });
+    
+    if (e.target.value.length === 0 && matchList) {
+            matchList.classList.remove('active')
+    } else if (matchList) {
+            matchList.classList.add('active');
+    }
+    
+    if (filteredUsers == 0 && users) {
+        showNoResult(users); 
+    }  else if (filteredUsers) {
+        displayUsers(filteredUsers);
+    }
+
+    /**
+      * this function capitalizes the first letter in some string
+      * @param string - string we are checking
+      */
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
+    /**
+      * If we press enter after we time an user it sends us to its page if there is one
+      */
+    if (e.keyCode === 13) {
+        webLink = `https://jsonplaceholder.typicode.com/users?username=${capitalizeFirstLetter(e.target.value)}`; 
+        if (linkTag) {
+            linkTag.setAttribute("href", webLink);
+            linkTag.click(); 
+        } 
+    }
+});
+
+/**
+  * We are fetching users.json and if we don't fetch them catching an error
   * @param searchText - The text wich we are filtering when we enter some users
   */
 const searchUsers = async searchText => {
-const response = await fetch('https://jsonplaceholder.typicode.com/users'),
+try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
       users = await response.json();
 
-    /**
-     *  Get matches to current text input
-     */
-    let matches = users.filter(user => {
-        const regex = new RegExp(`^${searchText}`, 'gi');
-        return user.username.match(regex);
-    });
-
-    if (searchText.length === 0) {
-        matches = [];
-        matchList.innerHTML = '';
-    }
-    outputHtml(matches);
+} catch (error) {
+    console.log(error);
+}  
 };
 
 /**
-  * Shows results in HTML
-  * @param matches - the users that match out current text input
+  * Creating Html and appending it for the users
+  * @param users - we can change this so we can call diferent users
   */
-const outputHtml = matches => {
-    if (matches.length > 0) {
-        const usersHtml = matches.map(match => `
+const displayUsers = users => {
+    if (users.length !== 0) {
+        const usersHtml = users.map(user => `
         <div class="card card-body userData js-userData">
-            <a class="fs-5 fw-bold js-userNameTag">${match.username}</a>
+            <a class="fs-5 fw-bold js-userNameTag">${user.username}</a>
         </div>
         `).join('');
     if (matchList) {
@@ -49,6 +85,7 @@ const outputHtml = matches => {
         if (userNameTag) {
             userNameTag.forEach(element => {
             element.setAttribute('href', webLink);
+            element.setAttribute('target', "_blank");
         });
         }
     }
@@ -64,4 +101,19 @@ if (searchInput) {
     searchInput.addEventListener('input', () => searchUsers (searchInput.value));  
 }
 
-
+/**
+  * We are creating an HTML that is called when there is no users
+  * @param array - the array we are checking for matches
+  */
+function showNoResult(array){
+    let listData;
+    if(array.length){
+        userValue = searchInput.value;
+        listData = `<div class="card card-body userData js-userData">
+        <a class="fs-5 fw-bold js-userNameTag">There is no results</a>
+        </div>`;
+    }else{
+      listData = array.join('');
+    }
+    matchList.innerHTML = listData;
+}
