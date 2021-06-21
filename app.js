e014d9a4774da58a3c609bc00e823564f3d5e6a0
Marko Goronjic <72967482+MaGo0506@users.noreply.github.com>
2021-06-21 22:05:49 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,115 +5,100 @@ let   webLink,
       linkTag = document.querySelector('.js-linkTag'),
       users = [];
 
-
 /**
-  * Here we are filtering our users even if we type in lowercase
+  * We are fetching json data and catching an error, so we can manipulate it
+  * Mapping users so we can search if there are users we can go to
   */
-searchInput.addEventListener('keyup', (e) => {
-    const targetValue = e.target.value.toLowerCase(),
-          filteredUsers =  users.filter((user) => {
-            return user.username.toLowerCase().startsWith(targetValue);
-          });
+searchInput.addEventListener('keyup', async (e) => {
+
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users?username=${formatString(searchInput.value)}`);
+    users = await response.json();
+
+  } catch (error) {
+    if (error) {
+        console.log(error);
+    }
+  }  
+
+    users = users.map((user)=>{
+      return user = `<div class="card card-body userData js-userData">
+      <a class="fs-5 fw-bold js-userNameTag">${searchInput.value}</a>
+      </div>`;
+      });
     
+    /**
+      * Showing matches when we type somethin in the search bar 
+      */  
     if (e.target.value.length === 0 && matchList) {
             matchList.classList.remove('active')
     } else if (matchList) {
             matchList.classList.add('active');
     }
     
-    if (filteredUsers == 0 && users) {
-        showNoResult(users); 
-    }  else if (filteredUsers) {
-        displayUsers(filteredUsers);
-    }
-
-    /**
-      * this function capitalizes the first letter in some string
-      * @param string - string we are checking
-      */
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+    showSuggestions(users);
 
     /**
       * If we press enter after we time an user it sends us to its page if there is one
       */
     if (e.keyCode === 13) {
-        webLink = `https://jsonplaceholder.typicode.com/users?username=${capitalizeFirstLetter(e.target.value)}`; 
+        webLink = `https://jsonplaceholder.typicode.com/users?username=${formatString(e.target.value)}`; 
         if (linkTag) {
             linkTag.setAttribute("href", webLink);
             linkTag.click(); 
         } 
     }
-});
+});   
 
 /**
-  * We are fetching users.json and if we don't fetch them catching an error
-  * @param searchText - The text wich we are filtering when we enter some users
+  * When some user is clicked sends us to it's webpage that has more data about him/her
+  * if there isnt it sends us to a page with the url that has a string that we clicked
   */
-const searchUsers = async searchText => {
-try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    users = await response.json();
-
-} catch (error) {
-    console.log(error);
-}  
-};
-
-/**
-  * Creating Html and appending it for the users
-  * @param users - we can change this so we can call diferent users
-  */
-const displayUsers = users => {
-    if (users.length !== 0) {
-        const usersHtml = users.map(user => `
-        <div class="card card-body userData js-userData">
-            <a class="fs-5 fw-bold js-userNameTag">${user.username}</a>
-        </div>
-        `).join('');
-    if (matchList) {
-        matchList.innerHTML = usersHtml;
-
-    /**
-      * When some user is clicked sends us to it's webpage that has more data about him/her
-      */
-    matchList.addEventListener('click', (e) => {
-        if (e.target.classList.contains('js-userNameTag') || e.target.classList.contains('js-userData')) {
-        webLink = `https://jsonplaceholder.typicode.com/users?username=${e.target.innerText}`;
-        const userNameTag = document.querySelectorAll('.js-userNameTag');
-        if (userNameTag) {
-            userNameTag.forEach(element => {
-            element.setAttribute('href', webLink);
-            element.setAttribute('target', "_blank");
+matchList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('js-userNameTag') || e.target.classList.contains('js-userData')) {
+      webLink = `https://jsonplaceholder.typicode.com/users?username=${formatString(e.target.innerText)}`;
+      const userNameTag = document.querySelectorAll('.js-userNameTag');
+      if (userNameTag) {
+          userNameTag.forEach(element => {
+          element.setAttribute('href', webLink);
+          element.setAttribute('target', "_blank");
         });
-        }
+      }
     }
-    })
-    } 
-    }
-};
+  })
+  
+/**
+  * this function capitalizes the first letter in some string and lowercase the rest
+  * @param string - string we are checking
+  */
+ function formatString(string) {
+  return string
+    .replace(/(\B)[^ ]*/g, match => (match.toLowerCase()))
+    .replace(/^[^ ]/g, match => (match.toUpperCase()));
+}
+
+/**
+  * We are showing our auto-suggestion list
+  * @param list - the array we are targeting
+  */
+function showSuggestions(list){
+  let listData;
+  if(!list.length){
+      userValue = searchInput.value;
+      listData = `<div class="card card-body userData js-userData">
+      <a class="fs-5 fw-bold js-userNameTag">${searchInput.value}</a>
+      </div>`;
+  } else {
+    listData = list.join('');
+  }
+  if (matchList) {
+    matchList.innerHTML = listData;
+  }
+}
 
 /**
   * Every time we type in text this event is called
   */
 if (searchInput) {
-    searchInput.addEventListener('input', () => searchUsers (searchInput.value));  
-}
-
-/**
-  * We are creating an HTML that is called when there is no users
-  * @param array - the array we are checking for matches
-  */
-function showNoResult(array) {
-    let listData;
-    if (array.length) {
-        userValue = searchInput.value;
-        listData = `<div class="card card-body userData js-userData">
-        <a class="fs-5 fw-bold js-userNameTag">There is no results</a>
-        </div>`;
-    } else {
-      listData = array.join('');
-    }
-    matchList.innerHTML = listData;
+    searchInput.addEventListener('input', () => searchInput.value);  
 }
